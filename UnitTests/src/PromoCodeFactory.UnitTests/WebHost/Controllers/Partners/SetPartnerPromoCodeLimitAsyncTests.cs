bbee7db5c17d79b8c46fc7a46cde9ed8cb86b65a
Aleinikov, Sergey A <sergey.aleinikov@kazzinc.com>
2024-09-02 13:59:26 +0500

    first two tests

--- UnitTests/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
+++ UnitTests/src/PromoCodeFactory.UnitTests/WebHost/Controllers/Partners/SetPartnerPromoCodeLimitAsyncTests.cs
@@ -1,7 +1,78 @@
-﻿namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
+﻿using System;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.Core.Domain.PromoCodeManagement;
+using PromoCodeFactory.WebHost.Controllers;
+using Xunit;
+
+namespace PromoCodeFactory.UnitTests.WebHost.Controllers.Partners
 {
     public class SetPartnerPromoCodeLimitAsyncTests
     {
-        //TODO: Add Unit Tests
+        private readonly Mock<IRepository<Partner>> _partnersRepositoryMock;
+        private readonly PartnersController _partnersController;
+        private readonly IFixture _fixture;
+
+        public SetPartnerPromoCodeLimitAsyncTests()
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _partnersRepositoryMock = _fixture.Freeze<Mock<IRepository<Partner>>>();
+            _partnersController = _fixture.Build<PartnersController>().OmitAutoProperties().Create();
+        }
+
+        //Если партнер не найден, то также нужно выдать ошибку 404;
+        [Fact]
+        public async void SetPartnerPromoCodeLimitAsync_PartnerIsNotFound_ReturnsNotFound()
+        {
+            // Arrange
+            var partnerId = Guid.Parse("def47943-7aaf-44a1-ae21-05aa4948b165");
+            Partner partner = null;
+
+            _partnersRepositoryMock.Setup(repo => repo.GetByIdAsync(partnerId))
+                .ReturnsAsync(partner);
+
+            // Act
+            var result = await _partnersController.SetPartnerPromoCodeLimitAsync(partnerId, null);
+
+            // Assert
+            result.Should().BeAssignableTo<NotFoundResult>();
+        }
+
+        // Если партнер заблокирован, то есть поле IsActive=false в классе Partner, то также нужно выдать ошибку 400;
+        [Fact]
+        public async void SetPartnerPromoCodeLimitAsync_WhenIsActiveEqualFalse_ReturnsNotFound()
+        {
+            // Arrange
+            var partnerId = Guid.Parse("def47943-7aaf-44a1-ae21-05aa4948b165");
+
+            var partner = _fixture.Build<Partner>()
+                .OmitAutoProperties()
+                .With(p => p.Id, partnerId)
+                .With(p => p.IsActive, false)
+                .Create();
+
+            _partnersRepositoryMock.Setup(repo => repo.GetByIdAsync(partnerId))
+                .ReturnsAsync(partner);
+
+            // Act
+            var result = await _partnersController.SetPartnerPromoCodeLimitAsync(partnerId, null);
+
+            // Assert
+            result.Should().BeAssignableTo<BadRequestObjectResult>();
+        }
+
+        // Если партнеру выставляется лимит, то мы должны обнулить количество промокодов, которые партнер выдал NumberIssuedPromoCodes, если лимит закончился, то количество не обнуляется;
+
+        // При установке лимита нужно отключить предыдущий лимит;
+
+        // Лимит должен быть больше 0;
+
+        // Нужно убедиться, что сохранили новый лимит в базу данных (это нужно проверить Unit-тестом);
+        // Если в текущей реализации найдутся ошибки, то их нужно исправить и желательно написать тест, чтобы они больше не повторялись.
+
     }
 }
